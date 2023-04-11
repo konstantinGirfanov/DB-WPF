464385d0905d71f489d57a07691dd08961ac442b
Kostya <super_serega_2007@mail.ru>
2023-04-11 20:28:31 +0500

    запихал классы для считывания схем в сам проект + пару тестовых схем с данным

--- DummyDatabase.Desktop/MainWindow.xaml.cs
+++ DummyDatabase.Desktop/MainWindow.xaml.cs
@@ -12,12 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DBCore;
 
 namespace DummyDatabase.Desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
